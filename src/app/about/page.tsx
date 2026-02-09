028bf626f9f3619112337725a8a3e8b1f6b84a7c
alamin islam <alamin.ndc.17@gmail.com>
2026-02-09 12:09:17 +0600

    updating

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,10 @@
-export default function AboutPage (){
-    return (
+import { setTimeout } from "timers"
+
+export default async function AboutPage (){
+    await new Promise((resolve)=> setTimeout (resolve,3000))
+
+    throw new Error ("some thing went wrong ")
+    return (    
         <div>
             <h2>This is About page</h2>
         </div>
